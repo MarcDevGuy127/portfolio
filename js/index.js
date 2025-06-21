f0ae7512694ae48c884108c9e204add4bdf6d7b4
MarcDevGuy127 <marceloh.github@gmail.com>
2025-06-21 18:54:12 -0300

    Adding title animated

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//selecionando elementos pelo id
+const tituloBanner = document.getElementById('tituloBanner');
+const headerH1 = tituloBanner.querySelector('h1');
+
+//definindo animacao por elementos e posicoes
+tituloBanner.addEventListener('mousemove', (evento) => {
+    const header = tituloBanner.getBoundingClientRect();
+    const mousePosX = evento.clientX - header.left;
+    const mousePosY = evento.clientY - header.top;
+
+    const headerPosX = header.width / 2;
+    const headerPosY = header.height / 2;
+
+    const moverX = (mousePosX - headerPosX) * 0.05;
+    const moverY = (mousePosY - headerPosY) * 0.05;
+
+    headerH1.style.transform = `translate(${moverX}px, ${moverY}px)`;
+});
+
+// 3. Quando o mouse sai do cabeçalho...
+tituloBanner.addEventListener('mouseleave', () => {
+    headerH1.style.transform = `translate(0px, 0px)`;
+});
